0be36677a3a154e4be11b814a5ac57956e3b3eb9
Andrii Banduliak <banduliak1303@gmail.com>
2022-11-21 21:49:46 +0300

    hw2 is done

---
+++ 1.cs
@@ -0,0 +1,30 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на 
+//выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+using static System.Console;
+Clear();
+
+int getDigit()
+{
+    int digit;
+    do
+    {
+        WriteLine("Введите положительное целое трехзначное число: ");
+        digit = int.Parse(Console.ReadLine());
+    } while (digit < 100 || digit > 999);
+    return digit;
+}
+void showDigit(int digit)
+{
+    WriteLine($"Вторая цифра этого числа: {(digit / 10) % 10}");
+}
+string choice;
+do
+{
+    showDigit(getDigit());
+    WriteLine("Еще число? введите (да), чтобы продолжить");
+    choice = Console.ReadLine();
+} while (choice == "да");
---
+++ 3.cs
@@ -0,0 +1,17 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день
+// недели, и проверяет, является ли этот день выходным.
+
+using static System.Console;
+Clear();
+
+bool isHoliday(int day) { return day>5 ? true : false; }
+int day;
+do
+{
+    Write("Enter a number for the day of the week: ");
+    day = int.Parse(ReadLine());
+} while (day > 7||day<1);
+
+Write($"Is this day off? ");
+if (isHoliday(day)) Write("Yes");
+else Write("No.");
